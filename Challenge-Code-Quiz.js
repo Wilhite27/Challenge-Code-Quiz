9d080f770d397209a40f9c7814648e5d990b7cd6
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-07-06 18:21:42 -0500

    timer-revision&develop-branch

---
+++ Challenge-Code-Quiz.js
@@ -0,0 +1,27 @@
+var timerEl = document.querySelector('#time-container')
+// var mainEl = document.getElementById('')
+var timeLeft = 90; 
+var timeInterval = setInterval(function() {
+    timeLeft--;
+    console.log(timeLeft);
+    timerEl.textContent= timeLeft
+    },1000);
+    // function timeStopInterval() 
+    // clearInterval(timeInterval)    
+    
+    const userchoice = document.addEventListener("click", e => 
+    {
+            const target = e.target;
+        
+            if (target.matches("#right-choice")) {
+                target.style.backgroundColor = "green";
+                console.log(userchoice-3);
+                window.alert("Correct!!!")
+                
+            } else if (target.matches("#wrong-choice")) { 
+                document.getElementById("wrong-choice").textContent;
+                document.getElementById("wrongchoiceprompt").innerHTML = "Wrong Answer!!!";
+                window.alert("Incorrect!!!")
+            }
+
+        });
